08dcded3a01510e2cacd2dc992641ec397098609
ha3a <harri.heikkila92@gmail.com>
2018-01-26 11:26:38 +0200

    korjailua

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,7 +1,7 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
 CREATE TABLE Raakaaine(
 id SERIAL PRIMARY KEY,
-nimi varchar(50) NOT NULL,
+nimi varchar(53) NOT NULL,
 kcalPer100 INTEGER NOT NULL,
 proteiiniPer100 float NOT NULL,
 hiilihydraatitPer100 float NOT NULL,
@@ -10,7 +10,7 @@ rasvaPer100 float NOT NULL
 
 CREATE TABLE Eines(
 id SERIAL PRIMARY KEY,
-nimi varchar(50) NOT NULL,
+nimi varchar(54) NOT NULL,
 kcal INTEGER NOT NULL,
 proteiini float NOT NULL,
 hiilihydraatit float NOT NULL,
@@ -19,21 +19,22 @@ rasva float NOT NULL
 
 CREATE TABLE Annos(
 id SERIAL PRIMARY KEY,
-nimi varchar(50) NOT NULL,
+raakaaine_id INTEGER REFERENCES Raakaaine(id),
+nimi varchar(55) NOT NULL,
 kcalPer100 INTEGER NOT NULL,
 proteiiniPer100 float NOT NULL,
 hiilihydraatitPer100 float NOT NULL,
 rasvaPer100 float NOT NULL
-raakaaine_id INTEGER REFERENCES Raakaaine(id)
 );
 
 CREATE TABLE Paivanravinto(
 id SERIAL PRIMARY KEY,
-nimi varchar(50) NOT NULL,
+annos_id INTEGER REFERENCES Annos(id),
+eines_id INTEGER REFERENCES Eines(id),
+nimi varchar(56) NOT NULL,
 kcal INTEGER NOT NULL,
 proteiini float NOT NULL,
 hiilihydraatit float NOT NULL,
 rasva float NOT NULL
-annos_id INTEGER REFERENCES Annos(id),
-eines_id INTEGER REFERENCES Eines(id),
+
 );
